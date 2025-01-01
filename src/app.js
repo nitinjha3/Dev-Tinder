5c76479da8906d96c2eb82d4098c40bd7a41caaa
nitinjha3 <nitinkumarvk100@gmail.com>
2025-01-01 20:36:12 +0530

    next in routing

--- src/app.js
+++ src/app.js
@@ -7,6 +7,19 @@ app.listen(3000,()=>{
     console.log("listing to PORT 3000!");
 })
 
+app.use("/user",(req,res,next)=>{
+    console.log("Handling the route user !!");
+    res.send("Response 1");
+    next();
+},
+(req,res)=>{
+    console.log("Handling the route user 2!!");
+    res.send("2nd Response!!");
+}
+);
+
+
+
 app.get("/",(req,res)=>{
     res.send("hello from server"); 
 })
@@ -15,4 +28,6 @@ app.get("/test/:id/:password",(req,res)=>{
     console.log(req.params);
     res.send("hello from test server");
 })
+
+
 
